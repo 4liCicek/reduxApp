0f81f9604216bca4c064db8775fbfd87b2c01d68
Ali ÇİÇEK <alicicek4151@gmail.com>
2023-01-24 12:04:11 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider, useSelector } from "react-redux";
+import DarkModeSwitcher from "./components/dark-mode/dark-mode-switcher";
+import LangSwitcher from "./components/lang-switcher/lang-switcher";
+import store from "./store";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark" : ""}>
+      <DarkModeSwitcher />
+      <LangSwitcher />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/helpers/locale-helper.js
@@ -0,0 +1,5 @@
+export const langs = [
+  { title: "English", code: "en", country: "gb" },
+  { title: "Türkçe", code: "tr", country: "tr" },
+  { title: "Francais", code: "fr", country: "fr" },
+];
